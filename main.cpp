fbb8db0be313a555c4e44e73a7c75daabb26075f
bankeruCTW <versaceadv69@gmail.com>
2024-12-02 15:34:06 +0200

    + tournament selection

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 #define MAX_INDIVIDS 100
 #define MAX_DELIVERIES 20   
 #define MAX_WEIGHT_TIR 25000
-#define MAX_GENERATIONS 10
+#define MAX_GENERATIONS 10000
+#define MAX_TOURNAMENT_SIZE 8
 
 std::random_device rd;
 std::mt19937 g(rd());
@@ -186,6 +187,7 @@ std::pair<std::unique_ptr<stIndivid>, std::unique_ptr<stIndivid>> crossover(cons
     auto child2 = std::make_unique<stIndivid>();
 
     size_t crossoverPoint = Helpers::getRandomNumber(0, MAX_DELIVERIES - 1);
+	size_t crossoverPoint2 = Helpers::getRandomNumber(crossoverPoint, MAX_DELIVERIES - 1);
     printf("Crossover point: %d\n", crossoverPoint);
 
     child1->objects.insert(child1->objects.end(), parent1.objects.begin(), parent1.objects.begin() + crossoverPoint);
@@ -231,21 +233,83 @@ std::pair<std::unique_ptr<stIndivid>, std::unique_ptr<stIndivid>> crossover(cons
     return { std::move(child1), std::move(child2) };
 }
 
-void selectParents(std::vector<std::unique_ptr<stIndivid>>& population, const std::vector<stObject>& objectList)
+void mutateIndividual(stIndivid& individ, float mutationRate) 
 {
-	constexpr uint8_t numParents = 8;
-
-    size_t p1 = Helpers::getRandomNumber(0, population.size() - 1);
-    size_t p2 = -1;
-    do 
+    for (auto& tir : individ.tirs) 
     {
-        p2 = Helpers::getRandomNumber(0, population.size() - 1);
-    } while (p1 == p2);
+        if ((float)rand() / RAND_MAX < mutationRate) 
+        {
+            if (tir.objects.size() > 1) 
+            {
+                size_t idx1 = rand() % tir.objects.size();
+                size_t idx2 = rand() % tir.objects.size();
+
+                while (idx1 == idx2) 
+                {
+                    idx2 = rand() % tir.objects.size();
+                }
+
+                std::swap(tir.objects[idx1], tir.objects[idx2]);
+                printf("Swapped objects %d and %d in Tir\n", idx1, idx2);
+            }
+        }
+    }
+
+    if ((float)rand() / RAND_MAX < mutationRate) 
+    {
+        size_t tir1 = rand() % individ.tirs.size();
+        size_t tir2 = rand() % individ.tirs.size();
+
+        while (tir1 == tir2) 
+        {
+            tir2 = rand() % individ.tirs.size();
+        }
+
+        if (!individ.tirs[tir1].objects.empty() && tir1 != tir2) 
+        {
+            size_t objIdx = rand() % individ.tirs[tir1].objects.size();
+            auto obj = individ.tirs[tir1].objects[objIdx];
+            individ.tirs[tir1].objects.erase(individ.tirs[tir1].objects.begin() + objIdx);
+            addObjectToTir(individ.tirs[tir2], obj, tir2);
+            printf("Moved object to Tir %zu\n", tir2);
+        }
+    }
+}
+
+auto createParent(std::vector<std::unique_ptr<stIndivid>>& population)
+{
+	std::vector<uint16_t> selectedParents;
+	
+    for (uint8_t i = 0; i < MAX_TOURNAMENT_SIZE; i++)
+	{
+		uint16_t randomParent = Helpers::getRandomNumber(0, population.size() - 1);
+		selectedParents.push_back(randomParent);
+	}
+	
+    std::sort(selectedParents.begin(), selectedParents.end(), [&population](uint16_t a, uint16_t b) {
+		return population[a]->fitness < population[b]->fitness;
+		});
+	
+    for (uint8_t i = 0; i < MAX_TOURNAMENT_SIZE; i++)
+		printf("Parent %d: %d (%.2f)\n", i, selectedParents[i], population[selectedParents[i]]->fitness);
+
+	return selectedParents[0];
+}
+
+void selectParents(std::vector<std::unique_ptr<stIndivid>>& population, const std::vector<stObject>& objectList, float mutationRate)
+{
+    size_t p1, p2;
+	p1 = createParent(population);
+	p2 = createParent(population);
+
 	printf("Selected parents: %d (%.2f) and %d (%.2f)\n", p1, population[p1]->fitness, p2, population[p2]->fitness);
     population[p1]->lifeSpan--;
 	population[p2]->lifeSpan--;
 
     auto [c1, c2] = crossover(*population[p1], *population[p2], objectList);
+
+    mutateIndividual(*c1, mutationRate);
+    mutateIndividual(*c2, mutationRate);
 
     calculateFitness(*c1);
     calculateFitness(*c2);
@@ -262,6 +326,34 @@ void selectParents(std::vector<std::unique_ptr<stIndivid>>& population, const st
         }), population.end());
 }
 
+float calculatePopulationDiversity(const std::vector<std::unique_ptr<stIndivid>>& population) {
+    std::set<float> uniqueFitnessValues;
+    for (const auto& individ : population)
+        uniqueFitnessValues.insert(individ->fitness);
+
+    return static_cast<float>(uniqueFitnessValues.size()) / population.size();
+}
+
+void adjustMutationRate(float& mutationRate, const std::vector<std::unique_ptr<stIndivid>>& population, float diversity) {
+    static int stagnationCounter = 0;
+    static float previousBestFitness = FLT_MAX;
+
+    float currentBestFitness = population[0]->fitness; 
+    if (fabs(currentBestFitness - previousBestFitness) < 0.001f) 
+        stagnationCounter++;
+    else 
+        stagnationCounter = 0;
+
+    previousBestFitness = currentBestFitness;
+
+    if (stagnationCounter > 10 || diversity < 0.1f) 
+        mutationRate = std::min(mutationRate * 1.2f, 1.f);
+    else 
+        mutationRate = std::max(mutationRate * 0.9f, 0.01f);
+
+    printf("Mutation rate adjusted to: %.2f\n", mutationRate);
+}
+
 int main()
 {
     static float bestFitness = FLT_MAX;
@@ -275,8 +367,16 @@ int main()
     for (auto& individ : population)
         calculateFitness(*individ);
 
-    for (uint16_t i = 0; i < MAX_GENERATIONS; i++)
-        selectParents(population, objectList);
+    float mutationRate = 0.05f;
+    float diversityThreshold = 0.1f;
+
+    for (uint16_t generation = 0; generation < MAX_GENERATIONS; generation++) 
+    {
+        printf("\n--- Generation %d ---\n", generation);
+        float diversity = calculatePopulationDiversity(population);
+        adjustMutationRate(mutationRate, population, diversity);
+        selectParents(population, objectList, mutationRate);
+    }
 
     for (size_t i = 0; i < population.size(); ++i) 
     {
@@ -285,7 +385,7 @@ int main()
         printf("Individ %d [fitness: %f, lifeSpan: %d]\n", i, population[i]->fitness, population[i]->lifeSpan);
     }
 
-	printf("Best fitness: %f\n", bestFitness);
+	printf("Best fitness: %f [%.2f mutation rate]\n", bestFitness, mutationRate);
     system("pause");
     return 0;
 }
